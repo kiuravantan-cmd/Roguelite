11a934b7ce2f937239a62da38b1ac119d5bdfb2b
kantu55 <49814798+kantu55@users.noreply.github.com>
2026-06-05 23:37:41 +0900

    敵の生成処理を実装

---
+++ Assets/MyProject/Scripts/InGame/Enemy/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TPSRoguelite.InGame.Enemy
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent navMeshAgent;
+        private Transform targetPlayer = null;
+
+        private void Awake ()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                targetPlayer = player.transform;
+            }
+            else
+            {
+                Debug.LogError("Playerタグのついたオブジェクトが見つかりません！");
+            }
+        }
+
+        private void Update ()
+        {
+            if (targetPlayer != null && navMeshAgent != null)
+            {
+                navMeshAgent.SetDestination(targetPlayer.position);
+            }
+        }
+    }
+}
