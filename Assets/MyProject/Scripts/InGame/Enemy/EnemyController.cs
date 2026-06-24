fce2816b3d4ff418c85ca7e143d1d00216518667
kantu55 <49814798+kantu55@users.noreply.github.com>
2026-06-25 01:19:08 +0900

    敵へのダメージリアクション追加

--- Assets/MyProject/Scripts/InGame/Enemy/EnemyController.cs
+++ Assets/MyProject/Scripts/InGame/Enemy/EnemyController.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,6 +8,7 @@ namespace TPSRoguelite.InGame.Enemy
     public class EnemyController : MonoBehaviour
     {
         private const string PLAYER_TAG_NAME = "Player";
+        private const float STUN_DURATION = 0.5f;
 
         /// <summary>
         /// NavMeshAgent
@@ -18,6 +21,8 @@ namespace TPSRoguelite.InGame.Enemy
         /// 目的地となるPlayerのTransform
         /// </summary>
         private Transform targetPlayer = null;
+
+        private CancellationTokenSource stunCts;
 
         private void Awake() 
         {
@@ -45,6 +50,53 @@ namespace TPSRoguelite.InGame.Enemy
             {
                 // プレイヤーの現在位置を毎フレーム目的地として設定する
                 navMeshAgent.SetDestination(targetPlayer.position);
+            }
+        }
+
+        private void OnEnable ()
+        {
+            enemyState.OnDamageAction -= HandleDamage;
+            enemyState.OnDamageAction += HandleDamage;
+        }
+
+        private void OnDisable ()
+        {
+            if (enemyState != null)
+            {
+                enemyState.OnDamageAction -= HandleDamage;
+            }
+
+            if (navMeshAgent != null && navMeshAgent.isActiveAndEnabled)
+            {
+                navMeshAgent.isStopped = false;
+            }
+        }
+
+        private void HandleDamage()
+        {
+            stunCts?.Cancel();
+            stunCts?.Dispose();
+            stunCts = null;
+            stunCts = new CancellationTokenSource();
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(stunCts.Token, this.GetCancellationTokenOnDestroy());
+
+            StunAsync(linkedCts.Token).Forget();
+        }
+
+        private async UniTaskVoid StunAsync (CancellationToken token)
+        {
+            if (navMeshAgent == null)
+            {
+                return;
+            }
+
+            navMeshAgent.isStopped = true;
+
+            bool isCanceled = await UniTask.Delay(System.TimeSpan.FromSeconds(STUN_DURATION)).SuppressCancellationThrow();
+
+            if (!isCanceled && navMeshAgent.isActiveAndEnabled)
+            {
+                navMeshAgent.isStopped = false;
             }
         }
     }
--- Assets/MyProject/Scripts/InGame/Enemy/EnemyState.cs
+++ Assets/MyProject/Scripts/InGame/Enemy/EnemyState.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 using UnityEngine.Events;
 using Core.Interface;
 using Core.MasterData;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 
 namespace TPSRoguelite.InGame.Enemy 
 {
     public class EnemyState : MonoBehaviour, IDamageable
     {
+        // 点滅する時間
+        private const float kFlashDuration = 0.1f;
+
+        [SerializeField] private Renderer[] modelRenderers;
+
+        private Color[] defaultColors;
+        private CancellationTokenSource flashCts;
+
+        public event UnityAction OnDamageAction;
+
+        public event UnityAction<EnemyState> OnReturnToPoolAction;
+
         public EnemyDataRecord EnemyDataAsset { get; private set; }
 
         /// <summary>
@@ -14,15 +28,26 @@ namespace TPSRoguelite.InGame.Enemy
         /// </summary>
         public int CurrentHP { get; private set; }
 
-        public event UnityAction<EnemyState> OnReturnToPoolAction;
-
         public void Initialize(ulong id)
         {
             EnemyDataAsset = MasterDataAccessor.Instance.GetById<EnemyDataRecord>(id);
+
+            if (modelRenderers != null)
+            {
+                defaultColors = new Color[modelRenderers.Length];
+                for (int i = 0; i < modelRenderers.Length; i++)
+                {
+                    if (modelRenderers[i] != null)
+                    {
+                        defaultColors[i] = modelRenderers[i].material.color;
+                    }
+                }
+            }
         }
 
         public void Setup()
         {
+            ResetColor();
             CurrentHP = EnemyDataAsset.MaxHp;
             gameObject.SetActive(true);
         }
@@ -38,9 +63,60 @@ namespace TPSRoguelite.InGame.Enemy
             CurrentHP -= damageAmount;
             Debug.Log($"{EnemyDataAsset.EnemyName}に{damageAmount}のダメージ！残りHP:{CurrentHP}");
 
-            if (CurrentHP <= 0)
+            if (CurrentHP > 0)
+            {
+                OnDamageAction?.Invoke();
+
+                flashCts?.Cancel();
+                flashCts?.Dispose();
+                flashCts = null;
+                flashCts = new CancellationTokenSource();
+                var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(flashCts.Token, this.GetCancellationTokenOnDestroy());
+
+                DamageFlashAsync(linkedCts.Token).Forget();
+            }
+            else
             {
                 Die();
+            }
+        }
+
+        private async UniTaskVoid DamageFlashAsync(CancellationToken token)
+        {
+            if (modelRenderers == null)
+            {
+                return;
+            }
+
+            foreach (var r in modelRenderers)
+            {
+                if (r != null)
+                {
+                    r.material.color = Color.red;
+                }
+
+                bool isCanceled = await UniTask.Delay(System.TimeSpan.FromSeconds(kFlashDuration), cancellationToken: token).SuppressCancellationThrow();
+
+                if (!isCanceled)
+                {
+                    ResetColor();
+                }
+            }
+        }
+
+        private void ResetColor()
+        {
+            if (modelRenderers == null || defaultColors == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < modelRenderers.Length; i++)
+            {
+                if (modelRenderers[i] != null)
+                {
+                    modelRenderers[i].material.color = defaultColors[i];
+                }
             }
         }
 
