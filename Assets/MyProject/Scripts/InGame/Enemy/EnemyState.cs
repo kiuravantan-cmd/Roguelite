413286ae6bad9222ab5d04825db59383415f4316
vantan_kiura <kiura.vantan@gmail.com>
2026-07-18 18:23:39 +0900

    ノックバック実装

--- Assets/MyProject/Scripts/InGame/Enemy/EnemyState.cs
+++ Assets/MyProject/Scripts/InGame/Enemy/EnemyState.cs
@@ -2,11 +2,34 @@ using UnityEngine;
 using UnityEngine.Events;
 using Core.Interface;
 using Core.MasterData;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 
 namespace TPSRoguelite.InGame.Enemy 
 {
     public class EnemyState : MonoBehaviour, IDamageable
     {
+        /// <summary>
+        /// 点滅時間
+        /// </summary>
+        private const float FLASH_DURATION = 0.1f;
+
+        /// <summary>
+        /// キャラクターのレンダラー
+        /// </summary>
+        [SerializeField] private Renderer[] modelRenderers;
+
+        /// <summary>
+        /// キャラクターの元々の色
+        /// </summary>
+        private Color[] defaultColors;
+
+        /// <summary>
+        /// 点滅するアニメーションのキャンセルトークン
+        /// </summary>
+        private CancellationTokenSource flashCts;
+
         /// <summary>
         /// 敵のデータ
         /// </summary>
@@ -19,9 +42,23 @@ namespace TPSRoguelite.InGame.Enemy
 
         public event UnityAction<EnemyState> OnReturnToPoolAction;
 
+        public event UnityAction OnDamageAction;
+
         public void Initialize(ulong id)
         {
             EnemyDataAsset = MasterDataAccessor.Instance.GetById<EnemyDataRecord>(id);
+
+            if (modelRenderers != null)
+            {
+                defaultColors = new Color[modelRenderers.Length];
+                for (int i = 0; i < modelRenderers.Length; i++)
+                {
+                    if (modelRenderers[i] != null)
+                    {
+                        defaultColors[i] = modelRenderers[i].material.color;
+                    }
+                }
+            }
         }
 
         public void Setup()
@@ -34,6 +71,7 @@ namespace TPSRoguelite.InGame.Enemy
 
             CurrentHP = EnemyDataAsset.MaxHP;
             gameObject.SetActive(true);
+            ResetColor();
         }
 
         public void TakeDamage(int damageAmount) 
@@ -47,7 +85,21 @@ namespace TPSRoguelite.InGame.Enemy
             CurrentHP -= damageAmount;
             Debug.Log($"{EnemyDataAsset.EnemyName}に{damageAmount}のダメージ！残りHP:{CurrentHP}");
 
-            if (CurrentHP <= 0)
+            if (CurrentHP > 0)
+            {
+                OnDamageAction?.Invoke();
+
+                flashCts?.Cancel();
+                flashCts?.Dispose();
+                flashCts = null;
+
+                flashCts = new CancellationTokenSource();
+                var linlkedCts = CancellationTokenSource.CreateLinkedTokenSource(
+                    flashCts.Token, this.GetCancellationTokenOnDestroy());
+
+                DamageFlashAsync(linlkedCts.Token).Forget();
+            }
+            else
             {
                 Die();
             }
@@ -58,6 +110,49 @@ namespace TPSRoguelite.InGame.Enemy
             Debug.Log($"{EnemyDataAsset.EnemyName}を倒しました");
             gameObject.SetActive(false);
             OnReturnToPoolAction?.Invoke(this);
+        }
+
+        /// <summary>
+        /// 色をリセット
+        /// </summary>
+        private void ResetColor()
+        {
+            if (modelRenderers == null || defaultColors == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < modelRenderers.Length; i++)
+            {
+                if (modelRenderers[i] != null)
+                {
+                    modelRenderers[i].material.color = defaultColors[i];
+                }
+            }
+        }
+
+        private async UniTaskVoid DamageFlashAsync(CancellationToken token) 
+        {
+            if (modelRenderers == null)
+            {
+                return;
+            }
+
+            foreach (var renderer in modelRenderers)
+            {
+                if (renderer != null)
+                {
+                    renderer.material.color = Color.red;
+                }
+            }
+
+            bool isCanceled = await UniTask.Delay(TimeSpan.FromSeconds(FLASH_DURATION),
+                cancellationToken: token).SuppressCancellationThrow();
+
+            if (!isCanceled)
+            {
+                ResetColor();
+            }
         }
     }
 }
